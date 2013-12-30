8d5ad6100cdbf10537d25602c41f2dc554f3e19f
goorzwick <vonzwick@gmail.com>
2013-12-30 16:33:20 +0200

    testing Heuristics

--- RunExpriment1.py
+++ RunExpriment1.py
@@ -9,7 +9,7 @@ from RunExpriment2 import game
 	Runing this script should output all of the raw information for experiment 1 as defined
 	in the assignment specification doc.
 	players compared should be defined each in a faile named 'BlokusAgent<choose the name>.py'.
-	Output should be to file 'output1.txt' (or csv). 
+	Output should be to file 'output1.txt' (or csv).
 	Alternatively, you may split the output into multiple files named 
 	'output1_<a relevant name>.txt' (or csv).
 '''
@@ -19,27 +19,33 @@ out = 'output1.txt' # (or csv)
 
 
 depths = [2]
-RUNS = 1
+RUNS = 3
 TIME = 60
 table = {}
 if __name__ == '__main__':
     for i in depths:
         for j in depths:
-            table[('Y', i, 'S', j)] = { -1: 0, 0: 0, 1: 0 }
-            table[('S', i, 'Y', j)] = { -1: 0, 0: 0, 1: 0 }
+            table[('YC', i, 'S', j)] = { -1: 0, 0: 0, 1: 0 }
+            table[('YP', i, 'S', j)] = { -1: 0, 0: 0, 1: 0 }
+            table[('YC+P', i, 'S', j)] = { -1: 0, 0: 0, 1: 0 }
+            table[('YCS+P', i, 'S', j)] = { -1: 0, 0: 0, 1: 0 }
             if i != j:
                 table[('S', i, 'S', j)] = { -1: 0, 0: 0, 1: 0 }
                 table[('Y', i, 'Y', j)] = { -1: 0, 0: 0, 1: 0 }
             for run in range(RUNS):
                 result = game(TIME, BlokusAgentMcBoogerballs, i, BlokusAgentSimple, j, \
-                        { 'timeManagement': TimeManagement.MORE_TIME_AT_THE_END, \
-                        'heuristicType': Heuristics.PARENT })
-                table[('Y', i, 'S', j)][result] += 1
+                        { 'heuristicType': Heuristics.CORNERS })
+                table[('YC', i, 'S', j)][result] += 1
+                result = game(TIME, BlokusAgentMcBoogerballs, i, BlokusAgentSimple, j, \
+                        { 'heuristicType': Heuristics.PARENT })
+                table[('YP', i, 'S', j)][result] += 1
                 #result = game(TIME, BlokusAgentSimple, i, BlokusAgentMcBoogerballs, j)
                 result = game(TIME, BlokusAgentMcBoogerballs, i, BlokusAgentSimple, j, \
-                        { 'timeManagement': TimeManagement.MORE_TIME_AT_THE_BEGINNING ,\
-                        'heuristicType': Heuristics.PARENT })
-                table[('S', i, 'Y', j)][result] += 1
+                        {'heuristicType': Heuristics.CORNERS | Heuristics.PARENT})
+                table[('YC+P', i, 'S', j)][result] += 1
+                result = game(TIME, BlokusAgentMcBoogerballs, i, BlokusAgentSimple, j, \
+                        {'heuristicType': Heuristics.CORNERS_TIMES_SQUARES | Heuristics.PARENT })
+                table[('YCS+P', i, 'S', j)][result] += 1
                 if i != j:
                     result = game(BlokusAgentSimple, i, BlokusAgentSimple, j)
                     table[('S', i, 'S', j)][result] += 1
